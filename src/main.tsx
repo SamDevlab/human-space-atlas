55e87a4b0c832cbf5eb25701482ebc5e36615cd6
Samuel Araújo <samusilvadev@gmail.com>
2026-08-18 06:28:56 -0300

    mount stage b intelligence workbench

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 import './styles.css'
+import './stage-b.css'
 import App from './App'
+import { StageBWorkbench } from './components/StageBWorkbench'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <StageBWorkbench />
   </StrictMode>,
 )
